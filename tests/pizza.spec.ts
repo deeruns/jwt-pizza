b5e2378875bc899435ca896f122b17a773881696
Davin Thompson <davindthomp@gmail.com>
2025-02-11 12:19:09 -0700

    admin profile navigation added

--- tests/pizza.spec.ts
+++ tests/pizza.spec.ts
@@ -119,3 +119,21 @@ test('buy pizza with login', async ({ page }) => {
 //   await expect(page.getByRole('main')).toContainText('0.008 ₿');
 });
 
+test('navigate to admin profile', async ({ page }) => { 
+  await page.goto('http://localhost:5173/');
+  await page.getByRole('link', { name: 'Login' }).click();
+  await page.getByRole('textbox', { name: 'Email address' }).click();
+  await page.getByRole('textbox', { name: 'Email address' }).fill('a@jwt.com');
+  await page.getByRole('textbox', { name: 'Password' }).click();
+  await page.getByRole('textbox', { name: 'Password' }).fill('admin');
+  await page.getByRole('button', { name: 'Login' }).click();
+  
+  await page.getByRole('link', { name: '常' }).click();
+  
+  await expect(page.getByRole('main')).toContainText('常用名字');
+  await expect(page.getByRole('main')).toContainText('a@jwt.com');
+  await expect(page.getByRole('main')).toContainText('admin');
+  await expect(page.getByRole('main')).toContainText('Here is your history of all the good times.');
+  await page.getByRole('link', { name: 'Logout' }).click();
+  });
+
